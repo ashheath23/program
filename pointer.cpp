69e6c884beb8765e3c58da6763f2e60009cb5984
Ashley <ashley@running.student.iastate.edu>
2016-05-18 09:59:05 -0500

    Initial commit

---
+++ pointer.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int *findMax(int arr[], int n);
+
+int main()
+{
+    int n,i,p;
+    cout << "Enter number of data values";
+    cin >> n;
+    int arr[n];
+    for( i = 0; i < n; i++){
+        cout << "Enter value" << i+1 << ":" ;
+        cin >> arr[i];
+    }
+    p=*findMax(arr,n);
+    cout << "The max value is: " << p;
+
+    return 0;
+
+}
+
+int *findMax(int data[], int n)
+{
+    int *max=data;
+    int i;
+    for (i = 1; i<n ; i++)
+    {
+        if(*max < *(max+1)) *max=*(max+i);
+    }
+    return max;
+}
